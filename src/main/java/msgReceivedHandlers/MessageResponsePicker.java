78bdc781a47a27a6bfb918b20de39217fa57a3af
Maexm <oleschenko.maxim@gmail.com>
2020-09-13 23:26:30 +0200

    Added VoiceGuard

--- src/main/java/msgReceivedHandlers/MessageResponsePicker.java
+++ src/main/java/msgReceivedHandlers/MessageResponsePicker.java
@@ -41,6 +41,7 @@ public final class MessageResponsePicker {
 
 		// ########## RESPONSE SETS ##########
 		this.middlewareBefore.add(new Logger(client, this.audioProvider, this.surveys, this.playerEventHandler));
+		this.middlewareBefore.add(new VoiceGuard(client, this.audioProvider, this.surveys, this.playerEventHandler));
 		this.middlewareBefore.add(new MusicRecommendation(client, this.audioProvider, this.surveys, this.playerEventHandler));
 		this.responseSet = new Megumin(client, this.audioProvider, this.surveys, this.playerEventHandler);
 	}
---
+++ src/main/java/msgReceivedHandlers/VoiceGuard.java
@@ -0,0 +1,30 @@
+package msgReceivedHandlers;
+
+import java.util.ArrayList;
+
+import discord4j.core.GatewayDiscordClient;
+import discord4j.voice.AudioProvider;
+import musicBot.AudioEventHandler;
+import snowflakes.ChannelID;
+import survey.Survey;
+
+public class VoiceGuard extends Middleware {
+
+    public VoiceGuard(GatewayDiscordClient client, AudioProvider audioProvider, ArrayList<Survey> surveys,
+            AudioEventHandler audioEventHandler) {
+        super(client, audioProvider, surveys, audioEventHandler);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected boolean handle() {
+        if(this.msgObject.getChannelId().equals(ChannelID.VOICE) && !this.isAuthorVoiceConnected()){
+            this.sendPrivateAnswer("Bitte verwende den VoiceChannel nur dann, wenn du auch wirklich in einem Voicechannel bist!\n"
+            + "Für alles andere sind die anderen Textkanäle gedacht."
+            +"\n\n"
+            +"Danke!");
+        }
+        return true;
+    }
+    
+}
