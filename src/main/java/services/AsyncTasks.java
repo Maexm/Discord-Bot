8d42afd436db022387d27c2b2fe8e3d0eaed5e10
Maexm <oleschenko.maxim@gmail.com>
2020-09-20 13:12:40 +0200

    Created async services & wait task

---
+++ src/main/java/services/AsyncTasks.java
@@ -0,0 +1,12 @@
+package services;
+
+import java.time.Duration;
+
+import reactor.core.publisher.Mono;
+
+public class AsyncTasks {
+    
+    public final static Mono<Void> wait(Duration duration){
+        return Mono.delay(duration).then();
+    }
+}
