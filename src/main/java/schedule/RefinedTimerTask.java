d4b0ee3c781b8b2736f68d3cb866b737229e463b
Maexm <oleschenko.maxim@gmail.com>
2020-11-15 01:36:39 +0100

    Suppress rawtypes warnings

--- src/main/java/schedule/RefinedTimerTask.java
+++ src/main/java/schedule/RefinedTimerTask.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import exceptions.IllegalMagicException;
 
+@SuppressWarnings("rawtypes")
 public abstract class RefinedTimerTask extends TimerTask {
     
     private final Long delay;
