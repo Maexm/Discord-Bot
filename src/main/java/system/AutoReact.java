e01a816255e6e146cc6cb1e6ebd6f29b2d138090
Maexm <oleschenko.maxim@gmail.com>
2020-11-25 19:39:37 +0100

    Created AutoReact Middleware

---
+++ src/main/java/system/AutoReact.java
@@ -0,0 +1,34 @@
+package system;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.reaction.ReactionEmoji;
+import discord4j.voice.AudioProvider;
+import musicBot.AudioEventHandler;
+import survey.Survey;
+
+public class AutoReact extends Middleware {
+	
+	private final ReactionEmoji emoji;
+	
+	public AutoReact(GatewayDiscordClient client, AudioProvider audioProvider, ArrayList<Survey> surveys,
+			AudioEventHandler audioEventHandler, Predicate<Message> mayAccept, ReactionEmoji emoji) {
+		super(client, audioProvider, surveys, audioEventHandler, mayAccept);
+		this.emoji = emoji;
+	}
+
+	@Override
+	protected boolean handle() {
+
+		this.msgObject.addReaction(this.emoji);
+		
+		return false;
+	}
+
+	
+    
+    
+}
