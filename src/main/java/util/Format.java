5aea3ea0ebeda7646a2709aaf047b5e557663c17
Maexm <oleschenko.maxim@gmail.com>
2020-11-28 15:31:44 +0100

    Capitalize first char & test

--- src/main/java/util/Format.java
+++ src/main/java/util/Format.java
@@ -6,4 +6,19 @@ public class Format {
         int ret = (int) (val * Math.pow(10, digits));
         return (1. * ret)/digits;
     }
+
+    public static String firstCharsCapitalized(String s, final String delimiter){
+        String ret = "";
+        String lower = s.toLowerCase();
+        String upper = s.toUpperCase();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == lower.charAt(i) && (i == 0 || s.charAt(i-1) == ' ')){
+                ret += upper.charAt(i);
+            }
+            else{
+                ret += s.charAt(i);
+            }
+        }
+        return ret;
+    }
 }
---
+++ src/test/java/testUtils/TestFormat.java
@@ -0,0 +1,25 @@
+package testUtils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import util.Format;
+import util.Pair;
+
+public class TestFormat {
+    
+    @Test
+    public void testCapitalizeFirstChars(){
+        ArrayList<Pair<String, String>> pairs = new ArrayList<>();
+
+        pairs.add(new Pair<String, String>("Es Passiert Nichts", "Es Passiert Nichts"));
+        pairs.add(new Pair<String, String>("alles wird groß", "Alles Wird Groß"));
+
+        for(Pair<String,String> pair : pairs){
+            assertEquals(pair.value, Format.firstCharsCapitalized(pair.key, " "));
+        }
+    }
+}
