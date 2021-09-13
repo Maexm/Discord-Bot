c17f53d569c9af745015593fa4830263cf65a39c
Maexm <oleschenko.maxim@gmail.com>
2021-09-13 22:08:36 +0200

    Fix Typo

--- src/main/java/system/Megumin.java
+++ src/main/java/system/Megumin.java
@@ -989,7 +989,7 @@ public class Megumin extends ResponseType {
 					.flatMap(user -> user.getPrivateChannel())
 					.flatMap(channel -> channel.createMessage(spec ->{
 						spec.setContent(Markdown.toSafeMultilineBlockQuotes("In "+Markdown.toBold(this.getGuildSecureName())+" im "+Markdown.toBold(voiceChannel.getName())+" VoiceChannel ist etwas los. Komm und sag Hallo!\n\n"
-							+"Schreib auf dem entsprechendem Server "+Markdown.toCodeBlock("MegUnfollow "+voiceChannel.getId().asString())+" um die Benachrichtigung auszuschalten!"));
+							+"Schreib auf dem entsprechenden Server "+Markdown.toCodeBlock("MegUnfollow "+voiceChannel.getId().asString())+" um die Benachrichtigung auszuschalten!"));
 					})).block();
 				}
 			}
